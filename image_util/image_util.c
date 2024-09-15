1afea6eb47362137348af3670649eae2e7705d1f
Ahelsel <helseltony@gmail.com>
2024-09-15 13:02:24 -0400

    initial commit, added lodepng

---
+++ image_util/image_util.c
@@ -0,0 +1,3 @@
+//
+// Created by Tony on 9/15/2024.
+//
---
+++ image_util/image_util.h
@@ -0,0 +1,12 @@
+//
+// Created by Tony on 9/15/2024.
+//
+
+#ifndef MOSAIC_MAKER_IMAGE_UTIL_H
+#define MOSAIC_MAKER_IMAGE_UTIL_H
+
+typedef struct {
+    // TODO: return struct to hold image data
+} imageData;
+
+#endif //MOSAIC_MAKER_IMAGE_UTIL_H
