94a7f4b1fcbcd2dd239fe17348a88b64a893645a
Ahelsel <helseltony@gmail.com>
2024-09-24 11:54:55 -0400

    finished headers

--- image_util/image_util.c
+++ image_util/image_util.c
@@ -5,6 +5,9 @@
 
 #include "image_util.h"
 
+typedef struct Image Image;
+typedef struct Color Color;
+
 int load_image(const char* filename, Image* img) {
 
 }
@@ -13,7 +16,9 @@ int save_image(const char* filename, const Image* img) {
 
 }
 
-int compute_average_color(const Image* img, int x, int y, int width, int height, Color* avg_color);
+int compute_average_color(const Image* img, int x, int y, int width, int height, Color* avg_color) {
+
+}
 
 void free_image(Image* img) {
 
